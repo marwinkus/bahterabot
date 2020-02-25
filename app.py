f034ac5465b65f3e976bc0cfd8d9a500fbd5a3d4
marwinkus <61217880+marwinkus@users.noreply.github.com>
2020-02-25 14:31:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def handle_message(event):
     if event.message.text.casefold() == "minta operator":
         line_bot_api.reply_message(event.reply_token, colong())
     elif event.message.text.casefold() == "mau gacha":
-	    line_bot_api.reply_message(event.reply_token, gacha())
+		message = TextSendMessage(text=gacha())
+	    line_bot_api.reply_message(event.reply_token, message)
 
 def gacha():
 	
