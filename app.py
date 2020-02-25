4ae56e49be46c57161a9e690c86453a673c4ea1e
marwinkus <61217880+marwinkus@users.noreply.github.com>
2020-02-25 22:05:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,9 @@ def gacha10():
 	cha9 = gacha()
 	cha10 = gacha()
 		
-	return 'Anda mendapatkan: {}, {}, {}, {}, {}, {}, {}, {}, {}, dan {}.' format(cha1, cha2, cha3, cha4, cha5, cha6, cha7, cha8, cha9, cha10)
+	fulltext = 'Anda mendapatkan: {}, {}, {}, {}, {}, {}, {}, {}, {}, dan {}.' format(cha1, cha2, cha3, cha4, cha5, cha6, cha7, cha8, cha9, cha10)
+	
+	return fulltext
 	
 def gacha6():
 	get = random.randint(1,10)
